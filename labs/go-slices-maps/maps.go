287a779fe589ec19b76e69d02055706f2758c32a
DiegoSolorzanoO <A01422048@itesm.mx>
2020-03-08 18:36:51 -0600

    Solve go-slices-maps
    Signed-off-by: DiegoSolorzanoO <A01422048@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	var words = make(map[string]int)
+	var list = strings.Fields(s)
+	for _, v := range list {
+		words[v] = words[v] + 1
+	}
+	return words
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,15 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var sli = make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		sli[i] = make([]uint8, dx)
+	}
+	return sli
+}
+
 func main() {
+	pic.Show(Pic)
 }
